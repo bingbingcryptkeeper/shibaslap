d9d8e3d730f880b4853d3aae588199350d260d9c
Anonymous <>
2021-11-21 13:08:27 +1300

    new hero text

--- components/Hero.js
+++ components/Hero.js
@@ -12,10 +12,16 @@ const Hero = () => {
               <h1 className="mb-4 font-weight-normal line-height-1_4">SHIBA SLAP</h1>
               <h2 className="mb-4 font-weight-normal line-height-1_4">ANTI-MEME MEME TOKEN</h2>
               <p className="text-muted mb-4 pb-2">
-                Missed doge? Missed shiba? Missed dogebonk?
-                <a className="span-link" href="https://poocoin.app/tokens/0xbc30d59aff36d7ade2faf8152fb1a66757410fee" target="_blank"> $SLAP</a> is a community driven crypto currency, with real world use. We seek to maximize the benefits for coin holders. Don't miss out. 
+                Shiba Slap 
+                <a className="span-link" href="https://poocoin.app/tokens/0xbc30d59aff36d7ade2faf8152fb1a66757410fee" target="_blank"> ($SLAP) </a> 
+                is a new community-driven DeFi token on the Binance Smart Chain that is an automatic liquidity providing protocol that pays out static rewards to holders.
               </p>
-              <a class="btn btn-gradient" href="https://poocoin.app/tokens/0xbc30d59aff36d7ade2faf8152fb1a66757410fee">Buy now</a>
+              <p className="text-muted mb-4 pb-2"> 
+                $SLAP is a community driven, meme powered crypto, available for all, but purchased by only the finest coin connoisseurs aka - slappers. 
+                Missed out on Shiba? Missed out on Dogebonk? Don’t miss out on $SLAP. 
+                <a className="span-link" href="https://poocoin.app/tokens/0xbc30d59aff36d7ade2faf8152fb1a66757410fee"> Find out how to purchase your ticket to the moon here.</a>
+              </p>
+              <a className="btn btn-gradient" href="https://poocoin.app/tokens/0xbc30d59aff36d7ade2faf8152fb1a66757410fee">Buy now</a>
             </div>
           </Col>
           <Col lg={6}>
