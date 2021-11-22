d308fe8166e9f325de6a441f404c3828f07b30c6
Anonymous <>
2021-11-22 18:40:30 +1300

    removed text mute

--- components/Hero.js
+++ components/Hero.js
@@ -21,14 +21,14 @@ class Hero extends React.Component {
           <Row className="align-items-center">
             <Col lg={6}>
               <div className="pr-lg-5 div-text">
-                <h1 className="mb-4 font-weight-normal line-height-1_4">SHIBA SLAP</h1>
+                <h1 className="hero-header">SHIBA SLAP</h1>
                 <h2 className="mb-4 font-weight-normal line-height-1_4">ANTI-MEME MEME TOKEN</h2>
-                <p className="text-muted mb-4 pb-2">
+                <p className="mb-4 pb-2">
                   Shiba Slap 
                   <a className="span-link" href="https://poocoin.app/tokens/0xbc30d59aff36d7ade2faf8152fb1a66757410fee" target="_blank"> ($SLAP) </a> 
                   is a new community-driven DeFi token on the Binance Smart Chain that is an automatic liquidity providing protocol that pays out static rewards to holders.
                 </p>
-                <p className="text-muted mb-4 pb-2"> 
+                <p className="mb-4 pb-2"> 
                   $SLAP is a community driven, meme powered crypto, available for all, but purchased by only the finest coin connoisseurs aka - slappers. 
                   We won't tell you how to buy it, because you already bonk, you already know how. Don't miss out. #shibaslap $SLAP
                 </p>
