9407324960a1798285b23155a0b8343749f5e6a0
Anonymous <>
2021-11-23 07:34:26 +1300

    removed cursor pointer from logo

--- components/Tokenomics.js
+++ components/Tokenomics.js
@@ -23,7 +23,6 @@ const Tokenomics = () => {
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
               <Image 
-                className='shib-slap'
                 src="/images/slaplogo1.png" // Route of the image file
                 height={300} // Desired size with correct aspect ratio
                 width={300} // Desired size with correct aspect ratio
