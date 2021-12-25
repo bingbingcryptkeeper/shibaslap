e9cae77cb5abb58cdd0159fc219916fe14e20533
Anonymous <>
2021-12-26 03:03:08 +1300

    added in audit

--- components/Docs.js
+++ components/Docs.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container } from "reactstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { whitepaper } from 'files/whitepaper.pdf'
-import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faFileAlt } from "@fortawesome/free-solid-svg-icons";
 
 class Docs extends React.Component {
   constructor(props) {
@@ -17,8 +17,8 @@ class Docs extends React.Component {
             <h1>Documents</h1>
           </div>
           <div className="wrapper">
-            <a 
-              href='files/whitepaper.pdf'
+            <a
+              href="files/whitepaper.pdf"
               alt="alt text"
               target="_blank"
               rel="noopener noreferrer"
@@ -36,8 +36,29 @@ class Docs extends React.Component {
                 </h5>
               </div>
             </a>
-            <a 
-              href='files/shibaslapflier.pdf'
+
+            <a
+              href="files/audit.pdf"
+              alt="alt text"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="docs-cards">
+                <div className="mb-5">
+                  <FontAwesomeIcon
+                    className="svg-tokenomics"
+                    icon={faFileAlt}
+                    color="#FB943E"
+                  />
+                </div>
+                <h5 className="text-dark font-weight-normal pt-1 mb-4">
+                  Audit
+                </h5>
+              </div>
+            </a>
+
+            <a
+              href="files/shibaslapflier.pdf"
               alt="alt text"
               target="_blank"
               rel="noopener noreferrer"
