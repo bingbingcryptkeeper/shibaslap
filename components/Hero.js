52d0419088fc459e551184aae8313f3226fa1f76
Anonymous <>
2022-01-03 23:33:05 +1300

    updated parooooograppppphhhhh

--- components/Hero.js
+++ components/Hero.js
@@ -36,11 +36,22 @@ class Hero extends React.Component {
                 <p className="mb-4 pb-2">
                   Shiba Slap 
                   <a className="span-link" href="https://poocoin.app/tokens/0xbc30d59aff36d7ade2faf8152fb1a66757410fee" target="_blank"> ($SLAP) </a> 
-                  is a new community-driven DeFi token on the Binance Smart Chain that is an automatic liquidity providing protocol that pays out static rewards to holders.
+                  is a new community-driven DeFi token on the Binance Smart Chain.
                 </p>
-                <p className="mb-4 pb-2"> 
-                  $SLAP is a community driven, meme powered crypto, available for all, but purchased by only the finest coin connoisseurs aka - slappers. 
-                  We won't tell you how to buy it, because you already bonk, you already know how. Don't miss out. #shibaslap $SLAP
+                <p className=""> 
+                  $SLAP was born to slab the shib out of so called 'meme' coins that have no memes. We meme, we slap, and we combine the two to produce a paw to be reckoned with.
+                </p>
+                <p className=""> 
+                  $SLAP is available to all, and all initiaties are designed to reward our token holders - not paid promoters and not the developers.
+                </p>
+                <p className=""> 
+                  So what are you waiting for? Pick yourself up a bag of slap, come join us on tele, reddit or twitter,  and don't miss out on being a foundational slapper.
+                </p>
+                <p className=""> 
+                  Contract address: 0xBC30d59aFF36D7AdE2faF8152fb1a66757410fEE
+                </p>
+                <p className=""> 
+                  #shibaslap $SLAP
                 </p>
               </div>
             </Col>
